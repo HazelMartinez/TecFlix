68eddc1de296a5b06236603048ab4350c70fea48
Hazel <hazel.martilo@gmail.com>
2019-09-12 21:31:55 -0600

    se agregan atributos a movie

--- TecFlix_Project/movie.cpp
+++ TecFlix_Project/movie.cpp
@@ -1,6 +1,59 @@
 #include "movie.h"
 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
 movie::movie()
 {
+    this->directorName = "";
+    this->num_critic_for_reviews = "";
+    this->duration = "";
+    this->director_facebook_likes= "";
+    this->actor_3_facebook_likes = "";
+    this->actor_2_name = "";
+    this->actor_1_facebook_likes = "";
+    this->gross = "";
+    this->genres = "";
+    this->actor_1_name = "";
+    this->movie_title = "";
+    this->num_voted_users = "";
+    this->cast_total_facebook_likes = "";
+    this->actor_3_name = "";
+    this->facenumber_in_poster = "";
+    this->plot_keywords = "";
+    this->movie_imdb_link = "";
+    this->num_user_for_reviews = "";
+    this->language = "";
+    this->country = "";
+    this->content_rating = "";
+    this->budget = "";
+    this->title_year = "";
+    this->actor_2_facebook_likes = "";
+    this->imdb_score = "";
+    this->aspect_ratio = "";
+    this->movie_facebook_likes = "";
+    this->completeLine = "";
 
 }
+
+void movie::muestra_nombre_pelicula_clik(){
+
+}
+
+void movie::muestra_nombre_autor_clik(){
+
+}
+
+void movie::muestra_nombre_duracion_clik(){
+
+}
+
+void movie::muestra_nombre_linkVideo_clik(){
+
+}
+
+void movie::obtener_link_video(){
+
+}
+
--- TecFlix_Project/movie.h
+++ TecFlix_Project/movie.h
@@ -5,7 +5,44 @@
 class movie
 {
 public:
+
+    char directorName;
+    char num_critic_for_reviews;
+    char duration;
+    char director_facebook_likes;
+    char actor_3_facebook_likes;
+    char actor_2_name;
+    char actor_1_facebook_likes;
+    char gross;
+    char genres;
+    char actor_1_name;
+    char movie_title;
+    char num_voted_users;
+    char cast_total_facebook_likes;
+    char actor_3_name;
+    char facenumber_in_poster;
+    char plot_keywords;
+    char movie_imdb_link;
+    char num_user_for_reviews;
+    char language;
+    char country;
+    char content_rating;
+    char budget;
+    char title_year;
+    char actor_2_facebook_likes;
+    char imdb_score;
+    char aspect_ratio;
+    char movie_facebook_likes;
+    char completeLine;
+
+
+    void obtener_link_video();
+    void muestra_nombre_pelicula_clik();
+    void muestra_nombre_autor_clik();
+    void muestra_nombre_linkVideo_clik();
+    void muestra_nombre_duracion_clik();
     movie();
+
 };
 
 #endif // MOVIE_H
